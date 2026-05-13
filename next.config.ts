c07711d7e6ad44d83957c4be2c9f3e8d17a2329f
MahesaSyawalA <mahesaabd4@gmail.com>
2026-05-13 19:59:06 +0700

    feat(admin): add automatic redirect from /admin to /admin/dashboard

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/dashboard',
+        permanent: true, 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
