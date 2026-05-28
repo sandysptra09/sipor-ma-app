ee4d7fb03b6cb86d450fc65d4986d427c42426bd
MahesaSyawalA <mahesaabd4@gmail.com>
2026-05-29 02:03:00 +0700

    feat: add get initial name helper

---
+++ lib/helpers/getInitialName.ts
@@ -0,0 +1,7 @@
+export const getInitialName = (name: string) => {
+    return name
+        ?.split(' ')
+        .slice(0, 2)
+        .map((word) => word.charAt(0).toUpperCase())
+        .join('');
+};
