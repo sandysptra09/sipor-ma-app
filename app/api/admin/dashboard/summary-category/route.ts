4864d9881ba50a0d123be2a5435b5a0d988f76ec
HypatiaG <ikhsannul201@gmail.com>
2026-05-28 21:38:44 +0700

    fix(category-summary): wrap GET endpoint logic in try-catch for error handling

--- app/api/admin/dashboard/summary-category/route.ts
+++ app/api/admin/dashboard/summary-category/route.ts
@@ -3,23 +3,29 @@ import  prisma  from "@/lib/prisma";
 import { auth } from "@/auth";
 
 export async function GET() {
-    const session = await auth();
-    if (!session || session.user.role !== "ADMIN") {
-        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    try{
+        const session = await auth();
+        if (!session || session.user.role !== "ADMIN") {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+    
+        const categoryGroups = await prisma.report.groupBy({
+            by: ["category"],
+            _count: { category: true },
+            orderBy: { _count: { category: "desc" } },
+        });
+    
+        const maxCount = await prisma.report.count();
+        const categories = categoryGroups.map((item) => ({
+            name: item.category,
+            count: item._count.category,
+            value: Math.round((item._count.category / maxCount) * 100),
+        }));
+    
+        return NextResponse.json({ categories });
+    }catch (error) {
+        console.error("Dashboard statistic error:", error);
+
+        return NextResponse.json({error: "Internal Server Error",},{ status: 500 });
     }
-
-    const categoryGroups = await prisma.report.groupBy({
-        by: ["category"],
-        _count: { category: true },
-        orderBy: { _count: { category: "desc" } },
-    });
-
-    const maxCount = await prisma.report.count();
-    const categories = categoryGroups.map((item) => ({
-        name: item.category,
-        count: item._count.category,
-        value: Math.round((item._count.category / maxCount) * 100),
-    }));
-
-    return NextResponse.json({ categories });
 }
